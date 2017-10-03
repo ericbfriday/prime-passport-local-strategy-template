0c852a1e673cf7249beb40578422709753f47bfe
Millie Walsh <millie@primeacademy.io>
2017-10-03 16:53:17 -0500

    delete instead of comment out

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,15 +3,6 @@ var router = express.Router();
 var passport = require('passport');
 var path = require('path');
 
-// Handles login form POST from index.html
-// router.post('/',
-//     passport.authenticate('local', {
-//         // request stays within node/express and is routed as a new request
-//         successRedirect: '/user',     // goes to routes/user.js
-//         failureRedirect: '/loginFailure'   // goes to get '/' route below
-//     })
-// );
-
 router.post('/', passport.authenticate('local'), function(req, res) {
   res.sendStatus(200);
 });
@@ -22,10 +13,5 @@ router.get('/', function(req, res) {
   console.log("request for index");
   res.sendFile(path.join(__dirname, '../public/views/index.html'));
 });
-
-// router.get('/*', function(req, res) {
-//   console.log("404 : ", req.params);
-//   res.sendStatus(404);
-// });
 
 module.exports = router;
